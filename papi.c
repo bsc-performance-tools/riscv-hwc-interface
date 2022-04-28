b9dda927dc84aca629c332407e3671e69830239d
Guillem Ramirez <guillem.ramirez@bsc.es>
2022-04-28 11:29:05 +0200

    Initial support for EPAC

--- papi.c
+++ papi.c
@@ -3,11 +3,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <limits.h>
+#include <sys/syscall.h>
 
+//#include "papi.h"
 #include <papi.h>
 
-#define MAX_HWC 4
-#define FIXED_HWC 2
+#define MAX_HWC 5
+#define FIXED_HWC 3
 #define MAX_CONF_HWC MAX_HWC - FIXED_HWC
 
 #if defined DEBUG
@@ -17,20 +19,20 @@
 #endif
 
 int   fake_PAPI_count_eventsets = 0;
-int **fake_PAPI_EventSets;
+uint64_t **fake_PAPI_EventSets;
 
 // p. 20 https://sifive.cdn.prismic.io/sifive%2F834354f0-08e6-423c-bf1f-0cb58ef14061_fu540-c000-v1.0.pdf
-struct RISCV_counter_def                                                                                                                                   
-{                                                                                                                                                          
+struct RISCV_counter_def
+{
 	unsigned int code;
 	char  name[32];
 	char  description[256];
-};                                                                                                                                                         
+};
 
-static enum riscv_hwc_map
+enum riscv_hwc_map
 {
-	INST_RETIRED = 50,
-	CYCLES = 59,
+	INST_RETIRED = 59,
+	CYCLES = 50,
 	EXC_TAKEN = 1,
 	INT_LD,
 	INT_ST,
@@ -86,7 +88,55 @@ static struct RISCV_counter_def riscv_hwc[] =
 
 	// Memory System Events
 	{ ((1 << 8)  | (2 & 0xFF)),  "INST_CACHE_MISS", "Instruction cache miss" },
-	{ ((1 << 9)  | (2 & 0xFF)),  "MEM_MAP_IO_ADDR", "Memory-mapped I/O access" }
+	{ ((1 << 9)  | (2 & 0xFF)),  "MEM_MAP_IO_ADDR", "Memory-mapped I/O access" },
+
+	//EPAC Old VPU Faltan los eventos por lane!!!
+	{0x020, "Load_Retries",                 "Number of Loads Retries" },
+	{0x040, "Complete_Vector_Instructions", "Number of Vector instructions finished at the VPU" },
+	{0x060, "Cycles_Stalled_VPU",           "Counts cycles that the VPU was stalled" },
+	{0x080, "Cycles_Stalled_Renaming",      "Counts cycles stalled at renaming due to lack of physical registers" },
+	{0x0A0, "Cycles_Stalled_Masked",        "Counts cycles stalled at the renaming due to the lack of physical registers for mask" },
+	{0x0C0, "Cycles_Stalled_Pre_Issue_Q",   "Counts cycles stalled at the Pre-Issue Queue" },
+	{0x0E0, "Cycles_Stalled_Unpacker",      "Counts cycles stalled at the Unpacker" },
+	{0x100, "Cycles_Stalled_ROB",           "Counts cycles stalled at the Re-Order Buffer" },
+	{0x120, "Cycles_Stalled_Arith_Q",       "Counts cycles stalled because Arithmetic Queue is full" },
+	{0x140, "Cycles_Stalled_Mem_Q",         "Counts cycles stalled because Memory Queue is full" },
+	{0x160, "Arithmetic_Instructions",      "Number of Arithmetic Instructions Issued" },
+	{0x180, "Memory_Instructions",          "Number of Memory Instructions Issued" },
+	{0x1A0, "Kill_Instructions",            "Number of Killed Instructions" },
+	{0x1C0, "Cycles_VPU_Active",            "Counts Cycles that the VPU was Active" },
+	{0x1E0, "Cycles_Empty_Pre_Issue_Q",     "Counts Cycles Pre-Issue Queue was Empty" },
+	{0x200, "Cycles_Empty_Unpacker",        "Counts Cycles Unpacker was Empty" },
+	{0x220, "Cycles_Empty_ROB",             "Counts Cycles Reorder Buffer was Empty" },
+	{0x240, "Cycles_Empty_Arithmetic_Q",    "Counts Cycles Arithmetic Queue was Empty" },
+	{0x260, "Cycles_Empty_Memory_Q",        "Counts Cycles Memory Queue was Empty" },
+
+	//EPAC New VPU
+	{ 0x020,  "COMPLETED_INST",  "Number of finished intructions at the VPU" },
+	{ 0x040,  "ISSUED_INST",     "Number of issued instructions at the VPU" },
+	{ 0x060,  "KILLS_COUNT",     "Number of kill events (not instructions) at the VPU" },
+	{ 0x080,  "LOAD_RETRIES",    "Number of load instructions with retries" },
+	{ 0x0A0,  "OVERLAPS_TAKEN",  "Number of instructions taking advantage of overlapping" },
+	{ 0x0C0,  "MEM_DEPEN",       "Number of memory instructions with dependencies (index or mask)" },
+	{ 0x0E0,  "VPU_ACTIVE",      "Number of cycles the VPU has been active" },
+	{ 0x100,  "VPU_STALLED",     "Number of cycles the VPU has been stalled" },
+	{ 0x120,  "RENAMING_STALL",  "Number of cycles stalled at renaming due to lack of physical registers" },
+	{ 0x140,  "MASKED_STALL",    "Number of cycles stalled at the renaming due to the lack of physical registers for mask" },
+	{ 0x160,  "PRE_ISSUE_STALL", "Number of cycles stalled at the Pre-Issue Queue" },
+	{ 0x180,  "UNPACKER_STALL",  "Number of cycles stalled at the Unpacker" },
+	{ 0x1A0,  "ROB_STALL",       "Number of cycles stalled at the Reorder Buffer" },
+	{ 0x1C0,  "ARITH_Q_STALL",   "Number of cycles stalled because the Arithmetic Queue is full" },
+	{ 0x1E0,  "MEMORY_Q_STALL",  "Number of cycles stalled because the Memory Queue is full" },
+	{ 0x200,  "PRE_ISSUE_EMPTY", "Number of cycles the Pre-Issue Queue has been empty" },
+	{ 0x220,  "UNPACKER_EMPTY",  "Number of cycles the Unpacker has been empty" },
+	{ 0x240,  "ROB_EMPTY",       "Number of cycles the Reorder Buffer has been empty" },
+	{ 0x260,  "ARITH_Q_EMPTY",   "Number of cycles the Arithmetic Queue has been empty" },
+	{ 0x280,  "MEMORY_Q_EMPTY",  "Number of cycles the Memory Queue has been empty" },
+	{ 0x2A0,  "ARITH_INST",      "Number of arithmetic instructions issued" },
+	{ 0x2C0,  "MEM_INST",        "Number of memory instructions issued" },
+	{ 0x2E0,  "OVERLAP_INST",    "Number of overlappable instructions issued" },
+	{ 0x300,  "FP_INST",         "Number of Floating-Point instructions issued" },
+	{ 0x320,  "FP_FMA_INST",     "Number of Floating-Point FMA instructions issued" }
 };
 
 #define CSR_reset(reg) asm volatile("csrc " #reg ", %0" :: "r" (-1))
@@ -100,41 +150,26 @@ static struct RISCV_counter_def riscv_hwc[] =
 	__tmp; \
 })
 
-#define CSR_configure(vals) \
+#define CSR_configure(num, val) \
 ({ \
-	CSR_write(mhpmevent3, vals[0]); \
-	CSR_write(mhpmevent4, vals[1]); \
+	int ret = syscall(__NR_arch_specific_syscall+20, 0x323+(num-3), val); \
 })
 
-static int rv_hwc_get_info(char *hwc, struct RISCV_counter_def *hwc_info)
-{
-	int i = 0;
-	for (i = 0; i<NUM_RISCV_HWC; i++)
-	{
-		if (strncmp(hwc, riscv_hwc[i].name, 32) == 0)
-		{
-			memcpy(hwc_info, &riscv_hwc[i], sizeof(struct RISCV_counter_def));
-			return 0;
-		}
+#define CSR_read_hpmcounter_case(num) \
+ case num:\
+	return CSR_read(hpmcounter##num);
+
+	//return CSR_read(hpmcounter##num);
+uint64_t CSR_read_hpmcounter(int counter_num){
+	switch (counter_num){
+		CSR_read_hpmcounter_case(3);
+		CSR_read_hpmcounter_case(4);
+		CSR_read_hpmcounter_case(5);
+		CSR_read_hpmcounter_case(6);
+		CSR_read_hpmcounter_case(7);
+		CSR_read_hpmcounter_case(8);
+		CSR_read_hpmcounter_case(9);
 	}
-
-return -1;
-} 
-
-void CSR_Configure(char **cnt)
-{
-	uint64_t hwc_events[MAX_CONF_HWC];
-	int i = 0;
-	for (i=0;i<MAX_CONF_HWC;i++)
-	{
-		struct RISCV_counter_def hwc_info;
-		if (rv_hwc_get_info(cnt[i], &hwc_info) == 0)
-		{
-			hwc_events[i] = hwc_info.code;
-		}
-	}
-
-	CSR_configure(hwc_events);
 }
 
 volatile
@@ -159,7 +194,7 @@ read_instret()
 	return tmp;
 }
 
-static long long i_hwc_values[FIXED_HWC] = { 0, 0 };
+static unsigned long long i_hwc_values[MAX_HWC] = { 0, 0, 0, 0, 0};
 
 int
 PAPI_start(int EventSet)
@@ -173,9 +208,9 @@ int
 PAPI_add_event(int EventSet, int EventCode)
 {
 	PRINT_DEBUG
-
-	int i = 0;
-	for (i = 0; i<FIXED_HWC; i++)
+	if (EventCode == CYCLES){fake_PAPI_EventSets[EventSet][0] = CYCLES; return PAPI_OK;}
+	if (EventCode == INST_RETIRED){fake_PAPI_EventSets[EventSet][2] = INST_RETIRED; return PAPI_OK;}
+	for (int i = 3; i < MAX_HWC; i++)
 	{
 		if (fake_PAPI_EventSets[EventSet][i] == 0)
 		{
@@ -205,10 +240,18 @@ int
 PAPI_event_code_to_name(int EventCode, char *EventName)
 {
 	PRINT_DEBUG
-	if (EventCode == INST_RETIRED) strncpy(EventName, "Instructions", strlen("Instructions")+1);
-	if (EventCode == CYCLES) strncpy(EventName, "Cycles", strlen("Cycles")+1);
+	if (EventCode == INST_RETIRED){ strncpy(EventName, "Instructions", strlen("Instructions")+1); return PAPI_OK;}
+	if (EventCode == CYCLES){ strncpy(EventName, "Cycles", strlen("Cycles")+1); return PAPI_OK;}
+	const int n = sizeof(riscv_hwc)/sizeof(riscv_hwc[0]);
+	for(int i = 0; i < n; i++){
+		if (EventCode == riscv_hwc[i].code){
+			strncpy(EventName, riscv_hwc[i].name, strlen(riscv_hwc[i].name)+1);
+			return PAPI_OK;
+		}
+ }
 
-	return PAPI_OK;
+	return PAPI_ENOEVNT;
+
 }
 
 int
@@ -219,9 +262,9 @@ PAPI_create_eventset(int *EventSet)
 	*EventSet = fake_PAPI_count_eventsets;
 
 	fake_PAPI_count_eventsets++;
-	fake_PAPI_EventSets = realloc(fake_PAPI_EventSets, fake_PAPI_count_eventsets*sizeof(int));
+	fake_PAPI_EventSets = realloc(fake_PAPI_EventSets, fake_PAPI_count_eventsets*sizeof(uint64_t));
 
-	fake_PAPI_EventSets[*EventSet] = calloc(FIXED_HWC, sizeof(int));
+	fake_PAPI_EventSets[*EventSet] = calloc(MAX_HWC, sizeof(uint64_t));
 
 	return PAPI_OK;
 }
@@ -237,10 +280,17 @@ int
 PAPI_event_name_to_code(const char *in, int *out)
 {
 	PRINT_DEBUG
-	if (strcmp(in, "Instructions") == 0) *out = INST_RETIRED;
-	if (strcmp(in, "Cycles") == 0) *out = CYCLES;
+	if (strcmp(in, "Instructions") == 0){ *out = INST_RETIRED; return PAPI_OK;}
+	if (strcmp(in, "Cycles") == 0){ *out = CYCLES; return PAPI_OK;}
+	const int n = sizeof(riscv_hwc)/sizeof(riscv_hwc[0]);;
+	for(int i = 0; i < n; i++){
+		if (strcmp(in, riscv_hwc[i].name) == 0){
+			*out = riscv_hwc[i].code; 
+			return PAPI_OK;
+		}
+ }
 
-	return PAPI_OK;
+	return PAPI_ENOEVNT;
 }
 
 int
@@ -250,19 +300,26 @@ PAPI_get_event_info(int EventCode, PAPI_event_info_t *info)
 	info->event_code = EventCode;
 	info->count = 1;
 
-	if (EventCode == INST_RETIRED)
-	{
+	if (EventCode == INST_RETIRED) {
 		strncpy(info->symbol, "Instructions", strlen("Instructions")+1);
 		strncpy(info->short_descr, "Instructions", strlen("Instructions")+1);
 		strncpy(info->long_descr, "Instructions", strlen("Instructions")+1);
-	}
-	if (EventCode == CYCLES)
-	{
+		return PAPI_OK;
+	}else if (EventCode == CYCLES) {
 		strncpy(info->symbol, "Cycles", strlen("Cycles")+1);
 		strncpy(info->short_descr, "Cycles", strlen("Cycles")+1);
 		strncpy(info->long_descr, "Cycles", strlen("Cycles")+1);
+		return PAPI_OK;
 	}
-
+	const int n = sizeof(riscv_hwc)/sizeof(riscv_hwc[0]);
+	for(int i = 0; i < n; i++){
+		if (EventCode == riscv_hwc[i].code){
+			strncpy(info->symbol, riscv_hwc[i].name, strlen(riscv_hwc[i].name)+1);
+			strncpy(info->short_descr, riscv_hwc[i].name, strlen(riscv_hwc[i].name)+1);
+			strncpy(info->long_descr, riscv_hwc[i].description, strlen(riscv_hwc[i].description)+1);
+			return PAPI_OK;
+		}
+ }
 	return PAPI_OK;
 }
 
@@ -327,17 +384,15 @@ PAPI_reset(int EventSet)
 	PRINT_DEBUG
 	
 	int i = 0;
-	for (i = 0; i<FIXED_HWC; i++)
+	for (i = 0; i<MAX_HWC; i++)
 	{
-		if (fake_PAPI_EventSets[EventSet][i] == INST_RETIRED)
-		{
-			//i_hwc_values[0] = CSR_read(instret);
+		if (fake_PAPI_EventSets[EventSet][i] == INST_RETIRED){
 			i_hwc_values[i] = read_instret();
-		}
-		else if (fake_PAPI_EventSets[EventSet][i] == CYCLES)
-		{
-			//i_hwc_values[1] = CSR_read(cycle);
+		}else if (fake_PAPI_EventSets[EventSet][i] == CYCLES){
 			i_hwc_values[i] = read_cycles();
+		}else if (fake_PAPI_EventSets[EventSet][i] != 0){
+			CSR_configure(i, fake_PAPI_EventSets[EventSet][i]);
+			i_hwc_values[i] = CSR_read_hpmcounter(i);
 		}
 	}
 
@@ -348,19 +403,16 @@ int
 PAPI_read(int EventSet, long long *hwc_values)
 {
 	PRINT_DEBUG
-
+	int count = 0;
 	int i = 0;
-	for (i = 0; i<FIXED_HWC; i++)
+	for (i = 0; i<MAX_HWC; i++)
 	{
-		if (fake_PAPI_EventSets[EventSet][i] == INST_RETIRED)
-		{
-			//hwc_values[0] = CSR_read(instret) - i_hwc_values[0];
-			hwc_values[i] = read_instret() - i_hwc_values[i];
-		}
-		else if (fake_PAPI_EventSets[EventSet][i] == CYCLES)
-		{
-			//hwc_values[1] = CSR_read(cycle) - i_hwc_values[1];
-			hwc_values[i] = read_cycles() - i_hwc_values[i];
+		if (fake_PAPI_EventSets[EventSet][i] == INST_RETIRED){
+			hwc_values[count++] = read_instret() - i_hwc_values[i];
+		}else if (fake_PAPI_EventSets[EventSet][i] == CYCLES){
+			hwc_values[count++] = read_cycles() - i_hwc_values[i];
+		}else if(fake_PAPI_EventSets[EventSet][i] != 0){
+			hwc_values[count++] = CSR_read_hpmcounter(i) - i_hwc_values[i];
 		}
 	}
 
@@ -371,34 +423,25 @@ int
 PAPI_accum(int EventSet, long long *hwc_values)
 {
 	PRINT_DEBUG
-	long long t_hwc_values[FIXED_HWC] = { 0, 0 };
-
+	unsigned long long t_hwc_values[MAX_HWC] = { 0, 0, 0, 0, 0 };
+	int count = 0;
 	int i = 0;
-	for (i = 0; i<FIXED_HWC; i++)
+	for (i = 0; i<MAX_HWC; i++)
 	{
-		if (fake_PAPI_EventSets[EventSet][i] == INST_RETIRED)
-		{
-			//t_hwc_values[0] = CSR_read(instret);
+		if (fake_PAPI_EventSets[EventSet][i] == INST_RETIRED){
 			t_hwc_values[i] = read_instret();
-			hwc_values[i] = t_hwc_values[i] - i_hwc_values[i];
-		}
-		else if (fake_PAPI_EventSets[EventSet][i] == CYCLES)
-		{
-			//t_hwc_values[1] = CSR_read(cycle);
+			hwc_values[count++] = (long long)(t_hwc_values[i] - i_hwc_values[i]);
+			i_hwc_values[i] = t_hwc_values[i];
+		}else if (fake_PAPI_EventSets[EventSet][i] == CYCLES){
 			t_hwc_values[i] = read_cycles();
-			hwc_values[i] = t_hwc_values[i] - i_hwc_values[i];
+			hwc_values[count++] = (long long)(t_hwc_values[i] - i_hwc_values[i]);
+			i_hwc_values[i] = t_hwc_values[i];
+		}else if(fake_PAPI_EventSets[EventSet][i] != 0){
+			t_hwc_values[i] = CSR_read_hpmcounter(i);
+			hwc_values[count++] = (long long)(t_hwc_values[i] - i_hwc_values[i]);
+			i_hwc_values[i] = t_hwc_values[i];
 		}
 	}
 
-	i_hwc_values[0] = t_hwc_values[0];
-	i_hwc_values[1] = t_hwc_values[1];
-
-	return PAPI_OK;
-}
-
-int
-PAPI_overflow(int EventSet, int EventCode, int threshold, int flags, PAPI_overflow_handler_t handler)
-{
-	PRINT_DEBUG
 	return PAPI_OK;
 }
--- test.c
+++ test.c
@@ -4,34 +4,115 @@
 
 #include <papi.h>
 
+volatile
+static inline uint64_t
+read_cycles()
+{
+	uint64_t tmp;
+	__asm__ __volatile__("rdcycle %0" : "=r"(tmp));
+	return tmp;
+}
+
+volatile
+static inline uint64_t
+read_instret()
+{
+	uint64_t tmp;
+	__asm__ __volatile__("rdinstret %0" : "=r"(tmp));
+	return tmp;
+}
+
+
+
 void main(int argc, char **argv)
 {
-	long long hwc_values[2] = {0, 0};
-
+	long long hwc_values[4] = {0};
+	int event_set=0;
+	PAPI_create_eventset(&event_set);
+	fprintf(stdout, "event_set = %lld\n", event_set);
 	PAPI_start(0);
+	PAPI_add_event(event_set, 50);
+	PAPI_add_event(event_set, 59);
+	int event_completed_inst=0;
+	int event_completed_inst2=0;
+	PAPI_event_name_to_code("ISSUED_INST", &event_completed_inst);
+	PAPI_event_name_to_code("ARITH_Q_STALL", &event_completed_inst2);
+	fprintf(stdout, "Complete_Vector_Instructions = %lld\n", event_completed_inst);
+	fprintf(stdout, "Cycles_VPU_Active = %lld\n", event_completed_inst2);
+	PAPI_add_event(event_set, event_completed_inst);
+	PAPI_add_event(event_set, event_completed_inst2);
+	PAPI_reset(0);
+	PAPI_accum(0, hwc_values);
+	PAPI_accum(0, hwc_values);
 
 	int i = 0;
 	int a = 4;
 
-	for (i=0; i < 10; i ++)
+	uint64_t tmp;
+	__asm__ __volatile__("rdcycle %0" : "=r"(tmp));
+	uint64_t scalar_cycles = tmp;
+	__asm__ __volatile__("rdinstret %0" : "=r"(tmp));
+	uint64_t scalar_instret = tmp;
+	for (i=0; i < 1000; i ++)
 	{
 		a += a;
 	}
-
-	PAPI_read(0, hwc_values);
-
-	fprintf(stdout, "instret = %lld\n", hwc_values[0]);
-	fprintf(stdout, "cycle = %lld\n", hwc_values[1]);
-
-	PAPI_reset(0);
-
-	for (i=0; i < 10; i ++)
-	{
-		a += a;
-	}
+	__asm__ __volatile__("rdcycle %0" : "=r"(tmp));
+	scalar_cycles = tmp - scalar_cycles;
+	__asm__ __volatile__("rdinstret %0" : "=r"(tmp));
+	scalar_instret = tmp - scalar_instret;
 
 	PAPI_accum(0, hwc_values);
 
-	fprintf(stdout, "instret = %lld\n", hwc_values[0]);
-	fprintf(stdout, "cycle = %lld\n", hwc_values[1]);
+	fprintf(stdout, "instret = %lld\n", scalar_instret);
+	fprintf(stdout, "cycle = %lld\n", scalar_cycles);
+	fprintf(stdout, "ipc = %lf\n", (double)scalar_instret/(double)scalar_cycles);
+
+	fprintf(stdout, "instret = %lld\n", hwc_values[1]);
+	fprintf(stdout, "cycle = %lld\n", hwc_values[0]);
+	fprintf(stdout, "ipc = %lf\n", (double)hwc_values[1]/(double)hwc_values[0]);
+	fprintf(stdout, "vec_instret = %lld\n", hwc_values[2]);
+	fprintf(stdout, "cycle_instret = %lld\n", hwc_values[3]);
+
+	PAPI_read(0, hwc_values);
+	PAPI_accum(0, hwc_values);
+
+	__asm__ __volatile__("rdcycle %0" : "=r"(tmp));
+	scalar_cycles = tmp;
+	__asm__ __volatile__("rdinstret %0" : "=r"(tmp));
+	scalar_instret = tmp;
+
+	asm("vsetvli zero, zero, e64, m1");
+	for (i=0; i < 1000; i ++){
+	asm("vfadd.vv v0, v0, v0");
+	asm("vfadd.vv v0, v0, v0");
+	asm("vfadd.vv v0, v0, v0");
+	asm("vfadd.vv v0, v0, v0");
+	asm("vfadd.vv v0, v0, v0");
+	asm("vfadd.vv v0, v0, v0");
+	asm("vfadd.vv v0, v0, v0");
+	asm("vfadd.vv v0, v0, v0");
+	asm("vfadd.vv v0, v0, v0");
+	asm("vfadd.vv v0, v0, v0");
+	asm("vfadd.vv v0, v0, v0");
+	asm("vfadd.vv v0, v0, v0");
+	}
+
+	__asm__ __volatile__("rdcycle %0" : "=r"(tmp));
+	scalar_cycles = tmp - scalar_cycles;
+	__asm__ __volatile__("rdinstret %0" : "=r"(tmp));
+	scalar_instret = tmp - scalar_instret;
+
+
+	PAPI_accum(0, hwc_values);
+
+	fprintf(stdout, "instret = %lld\n", scalar_instret);
+	fprintf(stdout, "cycle = %lld\n", scalar_cycles);
+	fprintf(stdout, "ipc = %lf\n", (double)scalar_instret/(double)scalar_cycles);
+
+	fprintf(stdout, "instret = %lld\n", hwc_values[1]);
+	fprintf(stdout, "cycle = %lld\n", hwc_values[0]);
+	fprintf(stdout, "ipc = %lf\n", (double)hwc_values[1]/(double)hwc_values[0]);
+	fprintf(stdout, "vec_instret = %lld\n", hwc_values[2]);
+	fprintf(stdout, "cycle_instret = %lld\n", hwc_values[3]);
 }
